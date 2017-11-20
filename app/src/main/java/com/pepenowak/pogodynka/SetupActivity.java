66ab5a8aea6834e1638ea0a44668b64da822824f
Piotr Nowak <dzobakpepe@gmail.com>
2017-11-20 19:04:30 +0100

    Add button, log clicks

--- app/src/main/java/com/pepenowak/pogodynka/SetupActivity.java
+++ app/src/main/java/com/pepenowak/pogodynka/SetupActivity.java
@@ -2,12 +2,27 @@ package com.pepenowak.pogodynka;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SetupActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(onStartClickListener);
     }
+
+    View.OnClickListener onStartClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d("SetupActivity", "kliknieto przycisk");
+
+        }
+    };
 }
